3c8c494816242b7ea0686f4e3874c2644d118e46
Lake on Wan <36900779+lakeonwan@users.noreply.github.com>
2023-10-03 12:58:56 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,28 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+//export default defineConfig({
+//  plugins: [
+//    vue(),
+//  ],
+//  resolve: {
+//    alias: {
+//      '@': fileURLToPath(new URL('./src', import.meta.url))
+//    }
+//  },
+//  base: '/',
+//})
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [vue()],
+    base: '/dist/',
   }
+
+  if (command !== 'serve') {
+    config.base = '/vue3-crash-course/dist/'
+  }
+
+  return config
 })
